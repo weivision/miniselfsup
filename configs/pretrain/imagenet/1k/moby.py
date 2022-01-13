a9cc05bf85feb37c593e4c5faa1a6da05bcdc20e
weivision <weilivision@exgmail.com>
2022-01-13 17:13:14 +0800

    [update] clean & format

--- configs/pretrain/imagenet/1k/moby.py
+++
@@ -1,5 +0,0 @@
-# ------------------------------------------------------------------------
-# miniSelfSup
-# Copyright (c) MMLab@NTU. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
-# ------------------------------------------------------------------------
